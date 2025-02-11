9cad85b10e7edb8e03122f435e0b86e528880869
mrpeanutma <connorwbstephens@gmail.com>
2025-02-11 13:56:43 -0500

    finished diff, untested

--- project_1/submission/src/main/java/uga/cs4370/mydb/RAImpl.java
+++ project_1/submission/src/main/java/uga/cs4370/mydb/RAImpl.java
@@ -103,7 +103,7 @@ public class RAImpl implements RA {
 
             // if no rowPresent after checking all rel1, insert into result
             if (!rowPresent) {
-                result.insert(rel2row)
+                result.insert(rel2row);
             }
 
         }
@@ -118,6 +118,67 @@ public class RAImpl implements RA {
      * @throws IllegalArgumentException If rel1 and rel2 are not compatible.
      */
     public Relation diff(Relation rel1, Relation rel2) {
+
+        // Check type compatibility
+        List<Type> rel1Types = new ArrayList<>(rel1.getTypes());
+        List<Type> rel2Types = new ArrayList<>(rel2.getTypes());
+        if (rel1Types.size() != rel2Types.size()) {
+            throw new IllegalArgumentException("rel1 and rel2 are incompatible.");
+        }
+
+        for (int i = 0; i < rel1Types.size(); i++) {
+            if (!rel1Types.get(i).equals(rel2Types.get(i))) {
+                throw new IllegalArgumentException("rel1 and rel2 are incompatible.");
+            }
+        }
+        
+        // Check attribute compatibility, may not be needed
+        List<String> commonAttr = new ArrayList<>(rel1.getAttrs());
+        commonAttr.retainAll(rel2.getAttrs());
+        if (commonAttr.size() != rel1Types.size()) {
+            throw new IllegalArgumentException("rel1 and rel2 are incompatible.");
+        }
+
+        // set up new relation based on rel1
+        Relation result = new RelationBuilder().attributeNames(rel1.getAttrs())
+                                               .attributeTypes(rel1.getTypes())
+                                               .build();
+
+        // for each row in rel1
+        for (int i = 0; i < rel1.getSize(); i++) {
+
+            List<Cell> rel1row = rel1.getRow(i);
+            boolean rowPresent = false;
+
+            // for each row in rel2
+            for (int j = 0; i < rel2.getSize() && rowPresent == false; j++) {
+
+                List<Cell> rel2row = rel2.getRow(j)
+                boolean rowMatch = true;
+
+                // for each cell in rel1, rel2, check equality
+                for (int k = 0; k < rel1row.size() && rowMatch; k++) {
+
+                    if (!rel1row.get(k).equals(rel2row.get(k))) {
+                        rowMatch = false;
+                    }
+                    
+                }
+                
+                // if rowMatch, then rowPresent
+                if (rowMatch == true) {
+                    rowPresent = true;
+                }
+                
+            }
+
+            // if no rowPresent after checking all rel2, insert into result
+            if (!rowPresent) {
+                result.insert(rel1row);
+            }
+
+        }
+        return result;
     };
 
     /**
