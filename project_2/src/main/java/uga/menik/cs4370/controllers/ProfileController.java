ce3fae3aecfd0cae93a3b1d2a4b4565ff2f17866
Will Gresham <willgresham34@gmail.com>
2025-03-22 14:42:35 -0400

    profile page

--- project_2/src/main/java/uga/menik/cs4370/controllers/ProfileController.java
+++ project_2/src/main/java/uga/menik/cs4370/controllers/ProfileController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import uga.menik.cs4370.models.Post;
+import uga.menik.cs4370.services.PostService;
 import uga.menik.cs4370.services.UserService;
 import uga.menik.cs4370.utility.Utility;
 
@@ -27,14 +28,16 @@ public class ProfileController {
 
     // UserService has user login and registration related functions.
     private final UserService userService;
+    private final PostService postService;
 
     /**
-     * See notes in AuthInterceptor.java regarding how this works 
+     * See notes in AuthInterceptor.java regarding how this works
      * through dependency injection and inversion of control.
      */
     @Autowired
-    public ProfileController(UserService userService) {
+    public ProfileController(UserService userService, PostService postService) {
         this.userService = userService;
+        this.postService = postService;
     }
 
     /**
@@ -50,19 +53,20 @@ public class ProfileController {
     /**
      * This function handles /profile/{userId} URL.
      * This serves the webpage that shows posts of a speific user given by userId.
-     * See comments in PeopleController.java in followUnfollowUser function regarding 
+     * See comments in PeopleController.java in followUnfollowUser function
+     * regarding
      * how path variables work.
      */
     @GetMapping("/{userId}")
     public ModelAndView profileOfSpecificUser(@PathVariable("userId") String userId) {
         System.out.println("User is attempting to view profile: " + userId);
-        
+
         // See notes on ModelAndView in BookmarksController.java.
         ModelAndView mv = new ModelAndView("posts_page");
 
         // Following line populates sample data.
         // You should replace it with actual data from the database.
-        List<Post> posts = Utility.createSamplePostsListWithoutComments();
+        List<Post> posts = postService.getPostByUserId(userId);
         mv.addObject("posts", posts);
 
         // If an error occured, you can set the following property with the
@@ -73,8 +77,8 @@ public class ProfileController {
         // Enable the following line if you want to show no content message.
         // Do that if your content list is empty.
         // mv.addObject("isNoContent", true);
-        
+
         return mv;
     }
-    
+
 }
--- project_2/src/main/java/uga/menik/cs4370/services/PostService.java
+++ project_2/src/main/java/uga/menik/cs4370/services/PostService.java
@@ -28,7 +28,6 @@ public class PostService {
     }
 
     public List<Post> getFollowedUsersPosts(String loggedInUser) {
-        // get followed users
         String sql = """
                     SELECT p.postId, p.postText, p.postDate,
                            u.userId, u.firstName, u.lastName,
@@ -47,8 +46,8 @@ public class PostService {
         try (Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            pstmt.setString(2, loggedInUser);
             pstmt.setString(1, loggedInUser);
+            pstmt.setString(2, loggedInUser);
             pstmt.setString(3, loggedInUser);
 
             try (ResultSet rs = pstmt.executeQuery()) {
@@ -74,8 +73,48 @@ public class PostService {
         return posts;
     }
 
-    public List<Post> getPostByUserId() {
-        return null;
-    };
+    public List<Post> getPostByUserId(String userId) {
+        String sql = """
+                    SELECT p.postId, p.postText, p.postDate,
+                           u.userId, u.firstName, u.lastName,
+                           (SELECT COUNT(*) FROM Heart h WHERE h.postId = p.postId) AS heartsCount,
+                           (SELECT COUNT(*) FROM Comment c WHERE c.postId = p.postId) AS commentsCount,
+                           (SELECT COUNT(*) FROM Heart h WHERE h.postId = p.postId AND h.userId = ?) AS isHearted,
+                           (SELECT COUNT(*) FROM Bookmark bm WHERE bm.postId = p.postId AND bm.userId = ?) AS isBookmarked
+                    FROM Post p, User u
+                    WHERE p.userId = ?
+                      AND p.userId = u.userId
+                    ORDER BY p.postDate DESC
+                """;
 
-}
+        List<Post> posts = new ArrayList<>();
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, userId);
+            pstmt.setString(2, userId);
+            pstmt.setString(3, userId);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    User user = new User(rs.getString("userId"), rs.getString("firstName"), rs.getString("lastName"));
+                    Post post = new Post(
+                            rs.getString("postId"),
+                            rs.getString("postText"),
+                            rs.getString("postDate"),
+                            user,
+                            rs.getInt("heartsCount"),
+                            rs.getInt("commentsCount"),
+                            rs.getInt("isHearted") > 0,
+                            rs.getInt("isBookmarked") > 0);
+                    posts.add(post);
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return posts;
+    }
+};
