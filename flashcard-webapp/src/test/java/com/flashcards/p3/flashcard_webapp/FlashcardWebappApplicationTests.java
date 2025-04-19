20e74e79affa115ffc8ff198e5660dbd2665984a
Will Gresham <willgresham34@gmail.com>
2025-04-18 20:14:58 -0400

    init project

---
+++ flashcard-webapp/src/test/java/com/flashcards/p3/flashcard_webapp/FlashcardWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashcards.p3.flashcard_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
