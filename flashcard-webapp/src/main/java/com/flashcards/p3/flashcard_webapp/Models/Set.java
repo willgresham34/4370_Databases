c3dc9b5a0a249980702f5ac62cfd098d87005c4a
Adam Wright <adambomb320@gmail.com>
2025-04-28 16:36:50 -0400

    card, user and set models

--- flashcard-webapp/src/main/java/com/flashcards/p3/flashcard_webapp/Models/Set.java
+++ flashcard-webapp/src/main/java/com/flashcards/p3/flashcard_webapp/Models/Set.java
@@ -1,5 +1,44 @@
 package com.flashcards.p3.flashcard_webapp.models;
+import java.util.ArrayList;
+
 
 public class Set {
+    /**
+     * Unique identifier for the set
+     */
+
+    private final String userId;
+    private final String setId;
+    private final String name;
+    private final String desc;
+    private final String category;
+    private final ArrayList <Flashcard> cards;
+
+
+    public Set(String userId, String setId, String name, String desc, String category, ArrayList <Flashcard> cards) {
+        this.userId = userId;
+        this.setId = setId;
+        this.name = name;
+        this.desc = desc;
+        this.category = category;
+        this.cards = cards;
+    }
+
+    public String getSetId() {
+        return setId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
 
 }
