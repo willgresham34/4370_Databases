e302b1b8f593c6ad264fa50faf5b6cd4ef8dd229
Adam Wright <adambomb320@gmail.com>
2025-04-29 00:07:35 -0400

    SetService implementation, added FolderService, other small changes

--- flashcard-webapp/src/main/java/com/flashcards/p3/flashcard_webapp/Services/SetService.java
+++ flashcard-webapp/src/main/java/com/flashcards/p3/flashcard_webapp/Services/SetService.java
@@ -1,5 +1,57 @@
 package com.flashcards.p3.flashcard_webapp.services;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+
+import com.flashcards.p3.flashcard_webapp.models.Flashcard;
+import com.flashcards.p3.flashcard_webapp.models.Set;
+
+@Service
+@SessionScope
 public class SetService {
+    private final DataSource dataSource;
+
+    @Autowired
+    public SetService(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    public boolean addSet(Set newSet, String userId) throws SQLException {
+        final String sql = "insert into Sets (userId, setName, setDescription, setCategory)";
+
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement setStmt = conn.prepareStatement(sql)) {
+                setStmt.setString(1, userId);
+                setStmt.setString(2, newSet.getName());
+                setStmt.setString(3, newSet.getDesc());
+                setStmt.setString(4, newSet.getCategory());
+
+                int rowsAffected = setStmt.executeUpdate();
+                return rowsAffected > 0;
+        }
+    }
+
+    public boolean addFlashcard(Flashcard newCard, String setId) throws SQLException {
+        final String sql = "insert into Flashcards (setId, cardTerm, cardDesc)";
+
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement cardStmt = conn.prepareStatement(sql)) {
+                cardStmt.setString(1, setId);
+                cardStmt.setString(2, newCard.getTerm());
+                cardStmt.setString(3, newCard.getcardDesc());
+
+                int rowsAffected = cardStmt.executeUpdate();
+                return rowsAffected > 0;
+
+        }
+    }
     
 }
